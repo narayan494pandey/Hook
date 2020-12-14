edadad5de20a095575bf81e26b217fbf837c5364
Narayan Pandey <npandey722@gmail.com>
2020-12-14 19:54:14 +0530

    useState prevState

--- src/HookCounttwo.js
+++ src/HookCounttwo.js
@@ -2,13 +2,19 @@ import React,{useState} from 'react'
 
 function HookCounttwo()  {
     const initial = 0;
-    const[count,setCount] = useState(initial)
+    const[count,setCount] = useState(initial);
+    const incrementFive = () =>{
+        for(let i=0;i<5;i++){
+            setCount(prevCount => prevCount+1);
+        }
+    }
     return (
         <div>
         Count:{count}
-            <button onClick={() => setCount(count+1)}>Increment</button>
+            <button onClick={() => setCount(prevCount => prevCount+1)}>Increment</button>
             <button onClick={() => setCount(initial)}>Reset</button>
-            <button onClick={() => setCount(count-1)}>Decrease</button>
+            <button onClick={() => setCount(prevCount => prevCount-1)}>Decrease</button>
+            <button onClick={incrementFive}>Increment5</button>
         </div>
     )
 }
