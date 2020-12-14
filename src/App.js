e948aebb0d282fe6401db0eac297b48b3b8f8c34
Narayan Pandey <npandey722@gmail.com>
2020-12-14 20:18:50 +0530

    useState with object

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css';
 import HookCounter from './HookCounter';
 import HookCounttwo from './HookCounttwo';
+import HookObject from './HookObject';
 
 function App() {
   return (
     <div className="App">
       <HookCounter />
       <HookCounttwo />
+      <HookObject />
         
     </div>
   );
